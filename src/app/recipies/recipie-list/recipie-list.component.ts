2e9bcfddb81f4cd0645914cb988bd26db2ddbba5
Vivek <vikichet@gmail.com>
2024-03-26 11:16:51 +0530

    list & edit component update

--- src/app/recipies/recipie-list/recipie-list.component.ts
+++ src/app/recipies/recipie-list/recipie-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from './recipe.model';
 export class RecipieListComponent implements OnInit {
 
   recipies: Recipe[] = [
-    new Recipe('Burger', 'Recipe for making a stacked spicy burger', 'https://img.freepik.com/free-vector/fresh-burger-fast-food-icon-isolated-design_18591-82739.jpg?t=st=1711307371~exp=1711310971~hmac=ddd708e7111f4abe61f7705fa70e9b5b42c2db37b92bc072caaed9021b1a167d&w=900')
+    new Recipe('Burger', 'Recipe for making a stacked spicy burger', 'https://img.freepik.com/free-vector/fresh-burger-fast-food-icon-isolated-design_18591-82739.jpg?t=st=1711307371~exp=1711310971~hmac=ddd708e7111f4abe61f7705fa70e9b5b42c2db37b92bc072caaed9021b1a167d&w=900'),
+    new Recipe('Speghetti', 'A delicious Speghetti recipe', 'https://img.freepik.com/free-vector/hand-drawn-food-cartoon-illustration_23-2150695329.jpg?t=st=1711379998~exp=1711383598~hmac=de8bd73b18d85a76123c6e6aa74966f6b2fe335cb259c77b96bec5164e222eee&w=826')
   ]
 
   ngOnInit(): void {
