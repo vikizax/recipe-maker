80984a66195f34db36b0fd54e6feb5d1038e404f
Vivek <vivek.chetia@clinifyhealth.com>
2024-04-01 16:28:40 +0530

    added HighlightDirective

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipiesComponent } from './recipies/recipies.component';
-import { RecipieListComponent } from './recipies/recipie-list/recipie-list.component';
 import { RecipieDetailsComponent } from './recipies/recipie-details/recipie-details.component';
 import { RecipieItemComponent } from './recipies/recipie-list/recipie-item/recipie-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipieListComponent } from './recipies/recipie-list/recipie-list.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { HighlightDirective } from './shared/dropdown.directive';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipieDetailsComponent,
     RecipieItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule
