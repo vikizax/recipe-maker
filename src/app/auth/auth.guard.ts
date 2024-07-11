c4ee696b962f1d84a5af1ff232710690d60b0ae8
Vivek <vivek.chetia@clinifyhealth.com>
2024-07-11 12:12:40 +0530

    updated auth guard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,12 +1,13 @@
 import { inject } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivateFn, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from "@angular/router";
-import { map } from "rxjs";
+import { map, take } from "rxjs";
 import { AuthService } from "./auth.service";
 
 export const AuthGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> => {
     const authService = inject(AuthService);
     const router = inject(Router);
     return authService.user.pipe(
+        take(1),
         map(user => {
             const isAuth = !!user;
             if (isAuth) return true;
